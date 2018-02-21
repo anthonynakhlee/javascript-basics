3648fbcc68c17ba6805d678f1b8190a886f881e2
anthony <anthony.na75@gmail.com>
2018-02-21 10:58:16 +0200

    this is step 3

---
+++ Base1/Step_3.js
@@ -0,0 +1,7 @@
+var person = prompt("Please enter your name",);
+
+if (person == null || person == "") {
+    alert("Is your name that classified?");
+} else {
+    alert("Hello " + person + "!");
+}
